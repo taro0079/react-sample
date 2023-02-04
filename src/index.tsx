dc288beb41181518c526e00f722723e8663d07fb
Taro Morita <morita0079@gmail.com>
2023-02-04 10:43:48 +0900

    add counter

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './components/Hello';
+import Message from './components/Message';
+import Parent from './components/ContainerSample';
+import Counter from './components/Counter';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello />
+    <Message />
+    <Parent />
+    <Counter initialValue={1} />
   </React.StrictMode>
 );
 
