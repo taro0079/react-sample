5d96e893fc338d60797b3b7a53c46d7cfc5a3487
Taro Morita <morita0079@gmail.com>
2023-02-04 10:50:50 +0900

    add newcounter

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import Hello from './components/Hello';
 import Message from './components/Message';
 import Parent from './components/ContainerSample';
 import Counter from './components/Counter';
+import NewCounter from './components/NewCounter';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,6 +18,7 @@ root.render(
     <Message />
     <Parent />
     <Counter initialValue={1} />
+    <NewCounter initialValue={1} />
   </React.StrictMode>
 );
 
